22c2f9f12578051165a57f4b3f61c6014bf4a26b
SSHayu2010 <meultra_seidlyn@hotmail.com>
2022-05-10 18:49:24 -0600

    cambio boss-ok

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!boss'):
+    if message.content.startswith('!ok'):
         global boss_hp
         await message.channel.send(f'A2 Current {boss_hp}')
 
