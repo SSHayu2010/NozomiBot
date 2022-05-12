b9ab8b940fa8a84ed26d8178ec3afb16c27f7019
SSHayu2010 <meultra_seidlyn@hotmail.com>
2022-05-11 21:41:57 -0600

    Se añadio flask

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 import discord
 from discord.ext import commands
+from webserver import keep_alive
+import os
 
 bot = commands.Bot(command_prefix="!", debug_guilds=[869923009115357215])
 
@@ -10,6 +12,10 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
 bot.load_extension('cogs.ranks')
+
+keep_alive()
+
+TOKEN = os.environ.get("DISCORD_BOT_SECRET_KEY")
 
 TOKEN = "OTczNDA4NDMxODE1OTQyMTY0.GCHG_r.LFh1560BiLYSMLEXB7qxaYb5n43dT2sjNlYCpw"
 bot.run(TOKEN)
