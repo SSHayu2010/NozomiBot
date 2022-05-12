e5a7dafe5b932e1ec666aafe98e24d424dc8a84f
Ivan <ivan.van@live.com>
2022-05-12 00:29:15 -0300

    Borrado de tipeos

--- main.py
+++ main.py
@@ -4,20 +4,6 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!", debug_guilds=[869923009115357215])
 
-boss_hp = 7500
-
-# all fields are optional
-embed = discord.Embed(title="Kokkoro - Rank Info", description="",
-                      colour=discord.Colour(0x6cfa4f))
-# change this link to an image link, e.g a cdn.discord image
-embed.add_field(name="Rank", value="10-2 (Abril, 2022)", inline=True)
-embed.add_field(name="Estrellas", value=":star: :star: :star: :star: :star:", inline=True)
-embed.add_field(name="Nivel UE", value="1", inline=True)
-embed.add_field(name="Restricciones / Notas adicionales", value="Requiere nivel 121 si aun la tienes", inline=True)
-embed.set_thumbnail(url="https://media.discordapp.net/attachments/972431199974871050/973136078305370124/kokkoro.png")
-embed.set_image(url="https://i.imgur.com/3UuPZFP.jpeg")
-embed.set_footer(text="AnneBot")
-
 
 @bot.event
 async def on_ready():
